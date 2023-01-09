7b120459c3f9d28616cbaed11b268ced2887af26
renggadiansa <renggadiansa@gmail.com>
2023-01-09 09:30:45 +0700

    add assets & codingan

--- app/src/main/java/com/example/customlistview/DetailedActivity.java
+++ app/src/main/java/com/example/customlistview/DetailedActivity.java
@@ -2,13 +2,35 @@ package com.example.customlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.customlistview.databinding.ActivityDetailedBinding;
+import com.example.customlistview.databinding.ActivityMainBinding;
+
 public class DetailedActivity extends AppCompatActivity {
+
+    ActivityDetailedBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed);
+        binding = ActivityDetailedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+        if (intent != null) {
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("time");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.drawable.maggi);
+
+            binding.detailedName.setText(name);
+            binding.detailedTime.setText(time);
+            binding.detailedDesc.setText(desc);
+            binding.detailedIngredients.setText(ingredients);
+            binding.detailedImage.setImageResource(image);
+        }
     }
 }
--- app/src/main/java/com/example/customlistview/MainActivity.java
+++ app/src/main/java/com/example/customlistview/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.customlistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
 import com.example.customlistview.databinding.ActivityMainBinding;
 
@@ -21,10 +24,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        int[] imageList = {};
-        int[] ingredientList = {};
-        int[] descList = {};
-        String[] nameList = {};
-        String[] timeList = {};
+        int[] imageList = {R.drawable.pasta, R.drawable.maggi, R.drawable.cake, R.drawable.pancake, R.drawable.pizza, R.drawable.burger, R.drawable.fries};
+        int[] ingredientList = {R.string.pastaIngredients, R.string.maggiIngredients,R.string.cakeIngredients,R.string.pancakeIngredients,R.string.pizzaIngredients, R.string.burgerIngredients, R.string.friesIngredients};
+        int[] descList = {R.string.pastaDesc, R.string.maggieDesc, R.string.cakeDesc,R.string.pancakeDesc,R.string.pizzaDesc, R.string.burgerDesc, R.string.friesDesc};
+        String[] nameList = {"Pasta", "Maggi", "Cake", "Pancake", "Pizza", "Burgers", "Fries"};
+        String[] timeList = {"30 mins", "2 mins", "45 mins", "10 mins", "60 mins", "45 mins", "30 mins"};
+
+        for (int i = 0; i < imageList.length; i++){
+            listData = new ListData(nameList[i], timeList[i], ingredientList[i], descList[i], imageList[i]);
+            dataArrayList.add(listData);
+        }
+        listAdapter = new ListAdapter(MainActivity.this, dataArrayList);
+        binding.listView.setAdapter(listAdapter);
+        binding.listView.setClickable(true);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(MainActivity.this, DetailedActivity.class);
+                intent.putExtra("name", nameList[i]);
+                intent.putExtra("time", timeList[i]);
+                intent.putExtra("ingredients", ingredientList[i]);
+                intent.putExtra("desc", descList[i]);
+                intent.putExtra("image", imageList[i]);
+                startActivity(intent);
+            }
+        });
     }
 }
